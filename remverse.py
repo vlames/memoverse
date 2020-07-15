dbffee938bfaa484ba4ff7122690ef163295cf1d
Vladimir Meshcheryakov <vlames@pdx.edu>
2020-07-14 23:44:43 -0500

    add the chunk of code to remove a verse from a database

--- remverse.py
+++ remverse.py
@@ -1,11 +1,19 @@
 # File: remverse.py
 # Description: the file contains code to direct a user to an html page and remove a chosen verse from the database
 
-from flask import redirect, url_for, render_template
+from flask import redirect, url_for, render_template, request
 from flask.views import MethodView
+import model
+
 class RemVerse(MethodView):
 	def get(self):
 		return render_template("remverse.html")
 	
 	def post(self):
+		reference = request.form["book-name"] + " " + request.form["book-chapter"] + ":" + request.form["book-verse"]
+		appmodel = model.get_model()
+		entries = [dict(reference=row[0], theme=row[1], verse=row[2], fums=row[3]) for row in appmodel.select()]
+		for entry in entries:
+			if reference.lower() == entry["reference"].lower():
+				appmodel.remove(entry["reference"])
 		return redirect(url_for('index'))
